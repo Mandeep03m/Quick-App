536bb999c7303c9bc957580bd10373ae5f434f18
Mandeep03m <mandeepsingh74575@gmail.com>
2025-10-30 23:40:23 +0530

    Update API URL for authentication

--- frontend/src/redux/slices/authSlice.js
+++ frontend/src/redux/slices/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 
-const API_URL = "https://quick-app-408j.onrender.com/auth";
+const API_URL = "https://quick-app-408j.onrender.com/api/auth";
 
 // Signup user
 export const signupUser = createAsyncThunk("auth/signup", async (formData, { rejectWithValue }) => {
